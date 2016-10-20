2093f006ff9ada818938f6defe8ba728b40e4995
Michael Doo <michael@410labs.com>
2016-10-20 10:55:32 -0400

    Don't delete refreshTokenExpirationDate if it's nil
    This fixes a bug in the TrustedPersistantOAuth2Session where, if the
    refreshToken is not nil, but the refreshTokenExpirationDate is, then the
    expiration date gets deleted from Keychain.

--- AeroGearOAuth2/TrustedPersistantOAuth2Session.swift
+++ AeroGearOAuth2/TrustedPersistantOAuth2Session.swift
@@ -379,11 +379,11 @@ public class TrustedPersistantOAuth2Session: OAuth2Session {
                 self.refreshToken = refreshToken
             }
 
-            if accessTokenExpirationDate != nil {
+            if accessToken != nil && accessTokenExpirationDate != nil {
                 self.accessTokenExpirationDate = accessTokenExpirationDate
             }
 
-            if refreshToken != nil {
+            if refreshToken != nil && refreshTokenExpirationDate != nil {
                 self.refreshTokenExpirationDate = refreshTokenExpirationDate
             }
     }
